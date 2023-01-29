b8089ced419035810ba48b4a1327ae4b6dcbefeb
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-29 11:38:39 -0300

    Mocks Tests Unitários

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+	
+	
+	@Test
+	public void DeveRetornarTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void DeveRetornarFalseParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void DeveRetornarTrueParaDataAtual() {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+
+}
